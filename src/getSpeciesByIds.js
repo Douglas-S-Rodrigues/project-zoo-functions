6744bc70d3e3df0bef9a060e5637a775b8ef2840
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-01-25 17:06:28 -0300

    funcao para getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-  
+// console.log(data.species.id)
+
+const { species } = require('../data/zoo_data');
+
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  return species.filter((animalId) => ids.includes(animalId.id)); // ideia de usar includes retirada desse link  https://desenvolvimentoparaweb.com/javascript/every-some-find-includes-javascript/
 }
 
 module.exports = getSpeciesByIds;
