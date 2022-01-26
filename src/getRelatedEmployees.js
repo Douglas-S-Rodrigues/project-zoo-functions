67eee537060b1f5c40ced5d0eae2267a817535c0
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-01-26 19:26:28 -0300

    funcao para getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
+console.log(data.employees);
+
+const { employees } = require('../data/zoo_data');
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employe) => employe.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return employees.filter((employe) => employe.managers.includes(managerId))
+      .map((name) => `${name.firstName} ${name.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
