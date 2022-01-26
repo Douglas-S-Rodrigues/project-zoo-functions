48338e2521f776ddb6e4d41ae75f30debfe13016
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-01-26 16:36:29 -0300

    criando funcao para getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { species } = require('../data/zoo_data');
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const listOfAnimals = species.filter((specie) => specie.name === animal);
+  const ageOfAnimals = listOfAnimals[0].residents.filter((specieAge) => specieAge.age < age);
+  if (ageOfAnimals.length === 0) {
+    return true;
+  } return false;
 }
 
 module.exports = getAnimalsOlderThan;
