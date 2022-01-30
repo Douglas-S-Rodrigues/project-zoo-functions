fb970984327d86b80436bfc6fe5b016212ad835a
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-01-30 16:18:45 -0300

    funcao para getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
+const animalsBySex = (options, residents) => residents
+  .filter((resident) => resident.sex === options.sex)
+  .map((resident) => resident.name);
+
+const animalsName = (options, residents) => {
+  let animals = residents.map((resident) => resident.name);
+  if (options.sex) {
+    animals = animalsBySex(options, residents);
+  }
+  if (options.sorted) {
+    animals = animals.sort();
+  }
+  return animals;
+};
 function getAnimalMap(options) {
-  // seu código aqui
+  const animalsLocation = {};
+  species.forEach(({ location }) => { animalsLocation[location] = []; });
+  if (options && options.includeNames) {
+    species.forEach(({ name, location, residents }) => {
+      const animalsSpecies = {};
+      animalsSpecies[name] = animalsName(options, residents);
+      animalsLocation[location].push(animalsSpecies);
+    });
+    return animalsLocation;
+  }
+  species.forEach(({ name, location }) => animalsLocation[location].push(name));
+  return animalsLocation;
 }
 
 module.exports = getAnimalMap;
