0602d13c49588efad4b9a48336c3b5ff95e6c8f6
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-01-30 15:04:26 -0300

    funcao para getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalsList = [];
+  const employeId = employees.filter((employe) => employe.id === id).map((resp) =>
+    resp.responsibleFor[0]);
+  const animals = species.filter((animal) => animal.id === employeId[0])[0].residents
+    .sort((ageA, ageB) => {
+      if (ageA.age < ageB.age) return 1;
+      if (ageA.age > ageB.age) return -1;
+      return 0;
+    });
+  animalsList[0] = animals[0].name;
+  animalsList[1] = animals[0].sex;
+  animalsList[2] = animals[0].age;
+  return animalsList;
 }
 
 module.exports = getOldestFromFirstSpecies;
